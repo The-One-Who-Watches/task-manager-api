4f3386df41987652637c3e0a539855920d27220d
James Lambourne <timeponyeon61@gmail.com>
2023-04-25 15:02:47 -0600

    Init commit

---
+++ src/mongodb.js
@@ -0,0 +1,33 @@
+// CRUD (Create, Read, Update, Delete)
+
+// const mongodb = require("mongodb");
+// const MongoClient = mongodb.MongoClient;
+// const ObjectID = mongodb.ObjectID;
+
+const { MongoClient, ObjectID } = require("mongodb");
+
+const connectionURL = "mongodb://127.0.0.1:27017";
+const databaseName = "task-manager";
+
+MongoClient.connect(
+  connectionURL,
+  { useNewUrlParser: true },
+  (error, client) => {
+    if (error) {
+      return console.log("unable to connect");
+    }
+
+    const db = client.db(databaseName);
+
+    db.collection("tasks")
+      .deleteOne({
+        description: "Write Nevelina Doctor Who",
+      })
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+);
